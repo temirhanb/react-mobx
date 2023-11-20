43684b3da33598f96290a413abbc434e8dc0c793
temirhanb <temirhan023@gmaul.com>
2023-11-20 16:19:19 +0500

    - wrong routing

--- src/pages/notFound/ui/index.tsx
+++ src/pages/notFound/ui/index.tsx
@@ -1,5 +1,7 @@
-export const NotFoundPage = ()=>{
+import React from "react";
+
+export const NotFoundPage:React.FC = ()=>{
   return(
-    <div className={'text-slate-500 text-xl m-10'}>Page not found D:</div>
+    <div data-testid={'not-found-page'} className={'text-slate-500 text-xl m-10'}>Page not found D:</div>
   )
 }
--- src/shared/test/router/router.test.js
+++ src/shared/test/router/router.test.js
@@ -3,24 +3,19 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from "@testing-library/react";
 import { App } from "../../../app/App";
-import { userEvent } from "@testing-library/user-event";
 
 jest.mock('axios')
 
 
 describe('Test router', () => {
 
-  test('page routing',  () => {
+  test('wrong routing', () => {
     render(
-      <MemoryRouter>
+      <MemoryRouter initialEntries={['/fdafs']}>
         <App/>
       </MemoryRouter>
     )
-    const peoplesPageLink = screen.getByTestId('link-peoples')
-    const favoritesPageLink = screen.getByTestId('link-favorites')
-    userEvent.click(favoritesPageLink)
-    expect(screen.getByTestId('favorite-page')).toBeInTheDocument()
-    userEvent.click(peoplesPageLink)
-    expect(screen.getByTestId('peoples-page')).toBeInTheDocument()
+
+    expect(screen.getByTestId('not-found-page')).toBeInTheDocument()
   })
 })
